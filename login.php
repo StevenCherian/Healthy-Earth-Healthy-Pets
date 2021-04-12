3c410443d55cf27587b369d73169612c0613851a
Steven Cherian <stevencherian.va@gmail.com>
2021-04-11 21:20:05 -0400

    Added basic login page

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	 	<link rel="stylesheet" href="styles.css">
+	 	<title>Healthy Earth, Healthy Pets</title>
+	
+	</head>
+	
+	<?php 
+	require_once('connection.php');
+	?>
+	
+	<body>
+	
+	<form method="post">
+	 
+		<div class ="login" id="log">
+			<h2><strong>Healthy Earth,</strong></h2>
+			<h2><strong>Healthy Pets</strong></h2>
+			
+			<label for="Email"><b>Email</b></label>
+			<input type="Email" placeholder="Email" name="Email" required>
+			
+			<label for="password"><b>Password</b></label>
+			<input type="Password" placeholder="Password" name="Password" required>
+			
+			<div style="display: inline; margin: 3% auto auto auto">
+				<button type="submit">Login</button>
+				<button type="button" onclick="signUp()">Don't have an accont? Sign up!</button>
+			</div>
+	 	</div>
+	 	
+	</form>
+	
+	<form method="post">
+	
+		<div class="sign_up" id="sign-up">
+			<h2><strong>Healthy Earth,<br>Healthy Pets</strong></h2>
+			
+			<label for="first_name"><b>First Name:</b></label>
+			<input type="text" placeholder="First Name" name="first_name" required>
+			
+			<label for="last_name"><b>Last Name:</b></label>
+			<input type="text" placeholder="Last Name" name="last_name" required>
+			
+			<label for="email"><b>Email:</b></label>
+			<input type="text" placeholder="Email" name="Email" required>
+			
+			<label for="password"><b>Password:</b></label>
+			<input type="text" placeholder="Password" name="Password" required>
+			
+			<div style="display: inline; margin: 3% auto auto auto">
+				<button type="submit">Login</button>
+				<button type="button" onclick="login()">Go back</button>
+			</div>
+		</div>
+	
+	</form>
+	
+	<script>
+		
+		function signUp() {
+			document.getElementById('sign-up').style.display='grid';
+			document.getElementById('log').style.display='none';
+		}
+		
+		function login() {
+			document.getElementById('log').style.display='grid';
+			document.getElementById('sign-up').style.display='none';
+		}
+	
+	</script>
+	
+	</body>
+</html>
