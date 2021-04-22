ff02002965cebc91ece14e55c6ad71b80f0cc7d4
Steven Cherian <stevencherian.va@gmail.com>
2021-04-21 21:47:28 -0400

    Login edits

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 	 	
 	 	<style>
     	 	body {
-                margin: 1%;
+                margin: 0;
                 margin-block: auto;
                 font-family: sans-serif;
                 animation: fadeInAnimation ease .6s;
@@ -27,7 +27,8 @@
             
             .myheader {
               padding: 3px 10px;
-              border-radius: 20px;
+              border-radius: 0px;
+              width: 100%;
               background: #5e5e5e;
               color: #f2f2f2;
             }
@@ -46,62 +47,63 @@
       		<h2>Healthy Earth, Healthy Pets</h2>
     	</div>
     	
-    	<form method="post">	 
-    		<div class ="login" id="log">
-    			<h2><strong>Log in to your account</strong></h2>
-    			
-    			<label for="Email"><b>Email</b></label>
-    			<input type="Email" placeholder="Email" name="Email" required>
-    			
-    			<label for="password"><b>Password</b></label>
-    			<input type="Password" placeholder="Password" name="Password" required>
-    			
-    			<div style="display: inline-grid;margin: 3% auto auto auto; grid-row-gap: 7px;">
-    				<button type="submit">Login</button>
-    				<button type="button" onclick="signUp()">Don't have an account? Sign up!</button>
-    			</div>
-    	 	</div>
-    	</form>
-    	
-    	<form method="post">
-    		<div class="sign_up" id="sign-up">
-    			<h2><strong>Create an account</strong></h2>
-    			
-    			<label for="first_name"><b>First Name:</b></label>
-    			<input type="text" placeholder="First Name" name="first_name" required>
-    			
-    			<label for="last_name"><b>Last Name:</b></label>
-    			<input type="text" placeholder="Last Name" name="last_name" required>
-    			
-				<label for="username"><b>Username::</b></label>
-    			<input type="text" placeholder="Username" name="Username" required>
-    			
-    			<label for="email"><b>Email:</b></label>
-    			<input type="text" placeholder="Email" name="Email" required>
-    			
-    			<label for="password"><b>Password:</b></label>
-    			<input type="text" placeholder="Password" name="Password" required>
-    			
-    			<div style="display: inline-grid;margin: 3% auto auto auto;grid-row-gap: 7px;">
-    				<button type="submit">Create Account</button>
-    				<button type="button" onclick="login()">Go back</button>
-    			</div>
-    		</div>
-    	</form>
-    	
-    	<script>
-    		
-    		function signUp() {
-    			document.getElementById('sign-up').style.display='grid';
-    			document.getElementById('log').style.display='none';
-    		}
-    		
-    		function login() {
-    			document.getElementById('log').style.display='grid';
-    			document.getElementById('sign-up').style.display='none';
-    		}
-    	
-    	</script>
-	
+    	<div class="login-container">
+        	<form method="post">	 
+        		<div class ="login" id="log">
+        			<h2><strong>Log in to your account</strong></h2>
+        			
+        			<label for="Email"><b>Email</b></label>
+        			<input type="Email" placeholder="Email" name="Email" required>
+        			
+        			<label for="password"><b>Password</b></label>
+        			<input type="Password" placeholder="Password" name="Password" required>
+        			
+        			<div style="display: inline-grid;margin: 3% auto auto auto; grid-row-gap: 7px;">
+        				<button type="submit">Login</button>
+        				<button type="button" onclick="signUp()">Don't have an account? Sign up!</button>
+        			</div>
+        	 	</div>
+        	</form>
+        	
+        	<form method="post">
+        		<div class="sign_up" id="sign-up">
+        			<h2><strong>Create an account</strong></h2>
+        			
+        			<label for="first_name"><b>First Name:</b></label>
+        			<input type="text" placeholder="First Name" name="first_name" required>
+        			
+        			<label for="last_name"><b>Last Name:</b></label>
+        			<input type="text" placeholder="Last Name" name="last_name" required>
+        			
+    				<label for="username"><b>Username::</b></label>
+        			<input type="text" placeholder="Username" name="Username" required>
+        			
+        			<label for="email"><b>Email:</b></label>
+        			<input type="text" placeholder="Email" name="Email" required>
+        			
+        			<label for="password"><b>Password:</b></label>
+        			<input type="text" placeholder="Password" name="Password" required>
+        			
+        			<div style="display: inline-grid;margin: 3% auto auto auto;grid-row-gap: 7px;">
+        				<button type="submit">Create Account</button>
+        				<button type="button" onclick="login()">Go back</button>
+        			</div>
+        		</div>
+        	</form>
+        	
+        	<script>
+        		
+        		function signUp() {
+        			document.getElementById('sign-up').style.display='grid';
+        			document.getElementById('log').style.display='none';
+        		}
+        		
+        		function login() {
+        			document.getElementById('log').style.display='grid';
+        			document.getElementById('sign-up').style.display='none';
+        		}
+        	
+        	</script>
+		</div>
 	</body>
 </html>
