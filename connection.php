3ab93a97eafe4ca5bddcceac05ba5f436a69b1f2
Steven Cherian <stevencherian.va@gmail.com>
2021-05-08 18:48:02 -0400

    connection

--- connection.php
+++ connection.php
@@ -36,28 +36,6 @@ if (!isset($_SESSION['user_ID']))
         // Verify password submitted by the user with the hash stored in the database
         if(!empty($queryResult) && password_verify($_POST["User_Password"], $queryResult['User_Password']))
         {
-//             if($queryResult['User_Type'] == "Veterinarian") {
-//                 $_SESSION['user_ID'] = $queryResult['UserID'];
-                
-//                 // Redirect to URL
-//                 header("Location: admin_main.php");
-                
-//             }
-            
-//             if($queryResult['User_Type'] == "SolarPowerComp") {
-//                 $_SESSION['user_ID'] = $queryResult['UserID'];
-                
-//                 // Redirect to URL
-//                 header("Location: solarpowerinfo.php");
-                
-//             }
-            
-//             if($queryResult['User_Type'] == "WindPowerComp") {
-//                 $_SESSION['user_ID'] = $queryResult['UserID'];
-                
-//                 // Redirect to URL
-//                 header("Location: windpowerinfo.php");
-//             } else {
                 // Create session variable
                 $_SESSION['user_ID'] = $queryResult['UserID'];
                 
