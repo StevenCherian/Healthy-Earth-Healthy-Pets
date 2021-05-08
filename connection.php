784eb5dc5c6e90e192589ee224ae44f268bc625a
Steven Cherian <stevencherian.va@gmail.com>
2021-05-07 23:52:04 -0400

    ugh

--- connection.php
+++ connection.php
@@ -36,6 +36,7 @@ if (!isset($_SESSION['user_ID']))
         // Verify password submitted by the user with the hash stored in the database
         if(!empty($queryResult) && password_verify($_POST["User_Password"], $queryResult['User_Password']))
         {
+            printf("%s\n", $queryResult['User_Type'] );
             if($queryResult['User_Type'] == "Veterinarian") {
                 $_SESSION['user_ID'] = $queryResult['UserID'];
                 
