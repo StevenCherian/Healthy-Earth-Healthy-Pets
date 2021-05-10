f6bf82375dbbd039b569c5102d33d37962c743e7
Steven Cherian <stevencherian.va@gmail.com>
2021-05-10 14:36:16 -0400

    pets

--- submitpetentry.php
+++ submitpetentry.php
@@ -5,14 +5,15 @@ require_once ('connection.php');
 try {
     
     $sqlQuery = "INSERT INTO Pet
-                     (Pet_Name, Species, Birthdate)
+                     (Pet_Name, Species, Birthdate, UserID)
                      VALUES
-                     (:petname, :species, :birthdate)";
+                     (:petname, :species, :birthdate, :UserID)";
     
     $stmt = $conn->prepare($sqlQuery);
     $stmt->bindValue(':petname', $_POST["petname"]);
     $stmt->bindValue(':species', $_POST["species"]);
     $stmt->bindValue(':birthdate', $_POST["birthdate"]);
+    $stmt->bindValue(':UserID', $_SESSION['user_ID']);
     $stmt->execute();
     
     header("Location: main.php");
