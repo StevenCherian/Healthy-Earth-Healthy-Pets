7f7e72e77446d1bd6b3247d65554d7b28abc12e2
Steven Cherian <stevencherian.va@gmail.com>
2021-05-09 20:12:01 -0400

    list power company info

---
+++ listpowercompanyinfo.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	 	<link rel="stylesheet" href="styles.css">
+	 	<title>Healthy Earth, Healthy Pets</title>
+	 	
+	 	<style>
+    	 	body {
+                margin: 0;
+                margin-block: auto;
+                font-family: sans-serif;
+                animation: fadeInAnimation ease .6s;
+                animation-iteration-count: 1;
+                animation-fill-mode: forwards;
+                background-color: #2a9d8f;
+            }
+
+            @keyframes fadeInAnimation {
+                0% { opacity: 0; }
+                100% { opacity: 1; }
+            }
+           
+            .header {
+              overflow: hidden;
+              background-color: #404040;
+              padding: 5px 1px;
+            }
+
+            .header a {
+              float: left;
+              color: white;
+              text-align: center;
+              padding: 12px;
+              text-decoration: none;
+              font-size: 15px; 
+              line-height: 15px;
+              border-radius: 4px;
+            }
+
+            .header a.logo {
+              font-size: 15px;
+              font-weight: bold;
+              color: white;
+            }
+
+            .header a.active {
+              background-color: dodgerblue;
+              color: white;
+            }
+
+            .header-right {
+              float: right;
+              padding-right: 12px;
+            }       
+
+            .main-container {
+                padding: 25px;
+                width: 95%;
+                margin: auto;
+                display: grid;
+                overflow: auto;
+                background-color: #f2f2f2;
+                color: #000000;
+                text-align: center;
+            }
+	 	 
+        </style>
+	</head>
+	
+	<body>
+	
+        <div class="header">
+          <a href="#default" class="logo">Healthy Earth, Healthy Pets</a>
+          <div class="header-right">
+            <a href="admin_main.php">Go back</a>
+          </div>
+        </div>
+    	
+	</body>
+</html>
+
+<?php
+
+require_once('connection.php');
+
+// Show all employees
+
+$stmt = $conn->prepare("SELECT Company_ID, Company_Name, Email_Address, Company_Phone_Number, Company_Address FROM Electricity_Supplier ORDER BY Company_Name");
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>Company ID</th><th>Company Name</th><th>Email Address</th><th>Phone Number</th><th>Address</th></tr></thead>";
+echo "<tbody>";
+
+while ($row = $stmt->fetch()) {
+    echo "<tr><td>$row[Company_ID]</td><td>$row[Company_Name]</td><td>$row[Email_Address]</td><td>$row[Company_Phone_Number]</td><td>$row[Company_Address]</td></tr>";
+}
+
+echo "</tbody>";
+echo "</table>";
+
+?>
