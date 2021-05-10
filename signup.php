a97d4594fc5b72524dfe8f13d5679ed00f9f0bd6
Steven Cherian <stevencherian.va@gmail.com>
2021-05-10 13:59:29 -0400

    singup

--- signup.php
+++ signup.php
@@ -38,7 +38,7 @@ try {
             $stmt = $conn->prepare("INSERT INTO Users VALUES (:User_Type, :First_Name, :Last_Name, :Email_Address, :User_Password)");
             $password = password_hash($_POST['User_Password'], PASSWORD_BCRYPT);
             
-            $stmt->bindValue(":User_Type", $name, PDO::PARAM_STR);
+            $stmt->bindParam(":User_Type", $name, PDO::PARAM_STR);
             $stmt->bindParam(":First_Name", $_POST['First_Name']);
             $stmt->bindParam(":Last_Name", $_POST['Last_Name']);
             $stmt->bindParam(":Email_Address", $_POST['Email_Address']);
