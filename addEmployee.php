1971e3e06a0d250aec65df8962692c7f3bc88166
Steven Cherian <stevencherian.va@gmail.com>
2021-05-10 16:20:12 -0400

    add emp

--- addEmployee.php
+++ addEmployee.php
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     
     echo "<select name='User_Type'>";
     
-    echo "<option value='-1'>Employee Must Have Type</option>";
+    echo "<option value='-1'>Emplyee Type Required</option>";
     
     while ($row = $stmt->fetch()) {
         echo "<option value='$row[User_Type]'>$row[User_Type]</option>";
@@ -128,7 +128,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     
     echo "<select name='ID'>";
     
-    echo "<option value='-1'>Employee Must Have Store ID</option>";
+    echo "<option value='-1'>Store ID Required</option>";
     
     while ($row = $stmt->fetch()) {
         echo "<option value='$row[ID]'>$row[ID]</option>";
