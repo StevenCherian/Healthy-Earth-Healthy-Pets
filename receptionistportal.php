aeaa84fc75616427df73d1f6223395b29ec98ed9
Steven Cherian <stevencherian.va@gmail.com>
2021-05-11 12:55:53 -0400

    receptionist portal

---
+++ receptionistportal.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<?php
+
+require_once 'connection.php';
+
+$stmt=$conn->prepare("SELECT UserID FROM Users WHERE UserID = :empID AND (User_Type = 'Receptionist')");
+$stmt->bindParam(":empID", $_SESSION['user_ID']);
+$stmt->execute();
+$admin = $stmt->fetch();
+
+if(!is_array($admin)){
+    header("Location: notauthorized.php");
+}
+?>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	 	<link rel="stylesheet" href="styles.css">
+	 	<title>Healthy Earth, Healthy Pets</title>
+	 	
+	 	<style>
+    	 	body {
+                margin: 0;
+                margin-block: auto;
+                font-family: sans-serif;
+                animation: fadeInAnimation ease .6s;
+                animation-iteration-count: 1;
+                animation-fill-mode: forwards;
+                background-color: #2a9d8f;
+            }
+
+            @keyframes fadeInAnimation {
+                0% { opacity: 0; }
+                100% { opacity: 1; }
+            }
+           
+            .header {
+              overflow: hidden;
+              background-color: #333333;
+              padding: 5px 1px;
+            }
+
+            .header a {
+              float: left;
+              color: white;
+              text-align: center;
+              padding: 12px;
+              text-decoration: none;
+              font-size: 15px; 
+              line-height: 15px;
+              border-radius: 4px;
+            }
+
+            .header a.logo {
+              font-size: 15px;
+              font-weight: bold;
+              color: white;
+            }
+
+            .header a.active {
+              background-color: dodgerblue;
+              color: white;
+            }
+
+            .header-right {
+              float: right;
+              padding-right: 12px;
+            }       
+
+            .main-container {
+                padding: 25px;
+                width: 95%;
+                margin: auto;
+                display: grid;
+                overflow: auto;
+                background-color: #f2f2f2;
+                color: #000000;
+                text-align: center;
+            }
+	 	 
+        </style>
+	</head>
+	
+	<body>
+	
+        <div class="header">
+          <a href="#default" class="logo">Healthy Earth, Healthy Pets</a>
+          <div class="header-right">
+            <a href="logout.php">Logout</a>
+          </div>
+        </div>
+    	
+    	<div class="main-container">
+    		<ul class="actions" style=list-style-type:none>
+	            <li><a href="listappointments.php">List all appointments</a></li>
+            </ul>
+        	<form method="post">	 
+
+        	</form>
+		</div>
+	</body>
+</html>
